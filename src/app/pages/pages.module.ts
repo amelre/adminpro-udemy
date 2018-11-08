c23ea0c90487133da8ddd9396a795a11d6e247be
a.melendezreyes <alejandra@vonus.mx>
2018-11-08 09:27:31 -0600

    Input, Output, ViewChild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,34 +1,47 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { PAGES_ROUTES } from './pages.routes';
 
 import { PagesComponent } from './pages.component';
-import { SharedModule } from "../shared/shared.module";
+import { SharedModule } from '../shared/shared.module';
+
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { ProgressComponent } from './progress/progress.component';
 
 
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
+
 
 
 @NgModule({
-    declarations:[
+    declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-    
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
-    exports:[
+    exports: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent
     ],
-    imports:[
+    imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
-export class PagesModule{}
+export class PagesModule { }
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -6,16 +6,16 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 
 
 
-const pagesRoutes : Routes = [
-    { 
-        path: '', 
+const pagesRoutes: Routes = [
+    {
+        path: '',
         component: PagesComponent,
-        children:[
+        children: [
             { path: 'dashboard', component: DashboardComponent },
             { path: 'progress', component: ProgressComponent },
             { path: 'graficas1', component: Graficas1Component },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-        ] 
+        ]
     },
 ];
 
